e218d211edeef8aed7b159dcfb15fcfb15d9d25d
HoneyBunny-exe <chirichvanya2510@gmail.com>
2024-02-15 22:39:38 +1000

    Add new models in data layer

---
+++ app/src/main/java/com/example/wordskillsbank/data/localStorage/model/TokenPairModel.kt
@@ -0,0 +1,3 @@
+package com.example.wordskillsbank.data.localStorage.model
+
+class TokenPairModel(val accessToken:String, val refreshToken:String)
--- app/src/main/java/com/example/wordskillsbank/data/localStorage/model/tokenPairModel.kt
+++
@@ -1,3 +0,0 @@
-package com.example.wordskillsbank.data.localStorage.model
-
-class tokenPairModel(val accessToken:String, val refreshToken:String)
