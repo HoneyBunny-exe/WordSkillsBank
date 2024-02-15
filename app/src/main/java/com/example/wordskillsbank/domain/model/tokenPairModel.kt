519edc2fc0b9535fdb695f76053484cf75a522ef
HoneyBunny-exe <chirichvanya2510@gmail.com>
2024-02-15 17:43:49 +1000

    test

---
+++ app/src/main/java/com/example/wordskillsbank/domain/model/tokenPairModel.kt
@@ -0,0 +1,4 @@
+package com.example.wordskillsbank.domain.model
+
+class tokenPairModel(val accessToken:String, val refreshToken:String) {
+}
---
+++ app/src/main/java/com/example/wordskillsbank/domain/usecase/SendAuthorizedDataUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.wordskillsbank.domain.usecase
+
+class SendAuthorizedDataUseCase {
+    fun execute(){
+
+    }
+}
