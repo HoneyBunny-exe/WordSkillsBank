bcce8ed2b9593fe63a0e0c84f4b0118c00756df4
HoneyBunny-exe <chirichvanya2510@gmail.com>
2024-02-16 19:23:20 +1000

    Completed the implementation of the basic usecase

--- app/src/main/java/com/example/wordskillsbank/domain/repository/ConfirmCodeRepository.kt
+++ app/src/main/java/com/example/wordskillsbank/domain/repository/ConfirmCodeRepository.kt
@@ -3,6 +3,7 @@ package com.example.wordskillsbank.domain.repository
 import com.example.wordskillsbank.domain.model.ConfirmCodeModel
 import com.example.wordskillsbank.domain.model.ResultModel
 import com.example.wordskillsbank.domain.model.TFATokenModel
+import com.example.wordskillsbank.domain.model.UserModel
 
 // Отправляет код подтверждения.
 // В зависимости от того, кто отправляет данные, вызовет нужный метод
@@ -13,4 +14,5 @@ interface ConfirmCodeRepository {
     fun sendPaymentConfirmCode(confirmCode: ConfirmCodeModel, tfaToken: TFATokenModel):Boolean
     fun sendNewLoginConfirmCode(confirmCode: ConfirmCodeModel, tfaToken: TFATokenModel):ResultModel
     fun sendNewPasswordConfirmCode(confirmCode: ConfirmCodeModel, tfaToken: TFATokenModel):ResultModel
+    fun sendCreateProfileConfirmCode(confirmCode: ConfirmCodeModel, tfaToken: TFATokenModel):UserModel
 }
--- app/src/main/java/com/example/wordskillsbank/domain/repository/UserModelRepository.kt
+++ app/src/main/java/com/example/wordskillsbank/domain/repository/UserModelRepository.kt
@@ -1,7 +1,10 @@
 package com.example.wordskillsbank.domain.repository
 
+import com.example.wordskillsbank.domain.model.RegistrationDataModel
+import com.example.wordskillsbank.domain.model.TFATokenModel
 import com.example.wordskillsbank.domain.model.UserModel
 
 interface UserModelRepository {
     fun getUserProfile():UserModel
+    fun createUserProfile(registrationDataModel: RegistrationDataModel):TFATokenModel
 }
