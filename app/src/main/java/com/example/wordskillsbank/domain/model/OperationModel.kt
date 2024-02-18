2cd681846fe36224bafce283b3c5f767f98db448
HoneyBunny-exe <chirichvanya2510@gmail.com>
2024-02-18 19:02:12 +1000

    Implemented operations api

---
+++ app/src/main/java/com/example/wordskillsbank/domain/model/OperationModel.kt
@@ -0,0 +1,9 @@
+package com.example.wordskillsbank.domain.model
+
+class OperationModel(
+    val status_operation: String,
+    val description: List<String>,
+    val about: String,
+    val currency: String,
+    val amount_money: String,
+    )
